f2a5a2cb6549edb96f5d9b11e0e61d4cd9f925bf
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-01-22 11:41:12 +0000

    Server setup:
    Declare variables:
    - express from 'express'
    - cors from 'cors'
    - bodyParser from 'body-parser'
    - app set use express
    - port set to 8080
    
    Use app:
    - cors()
    - listen on port 8080
    - listen
    
    Install dependancies:
    - body-parser
    - cors
    - express

--- server.js
+++ server.js
@@ -2,17 +2,28 @@
 projectData = {}
 
 // Require Express to run server and routes
+const express = require("express")
 
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Cors for cross origin allowance
+const cors = require("cors")
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static("website"))
 
+// Declare port
+const port = 8080
+
 // Setup Server
+app.listen(port, () => {
+  console.log("server running on localhost, port:", port)
+})
