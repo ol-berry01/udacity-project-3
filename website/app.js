378f561fdd65782ed7e1532eec5f8bf0650fa864
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-01-22 11:52:09 +0000

    Update app.js
    Declare variables:
    - zipCode equal to id of zip element
    - feelings equal to id of feelings element
    - generateBtn equal to id of generate element
    - zipCode equal to id of zip element
    - date equal to id of date element
    - temp equal to id of temp element
    - content equal to id of content element
    - month equal to array of month short names
    
    Updated variables:
    - newDate makes use of month array

--- website/app.js
+++ website/app.js
@@ -1,5 +1,12 @@
 /* Global Variables */
+const zipCode = document.querySelector("#zip")
+const feelings = document.querySelector("#feelings")
+const generateBtn = document.querySelector("#generate")
+const date = document.querySelector("#date")
+const temp = document.querySelector("#temp")
+const content = document.querySelector("#content")
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 // Create a new date instance dynamically with JS
 let d = new Date()
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear()
+let newDate = month[d.getMonth()] + "." + d.getDate() + "." + d.getFullYear()
