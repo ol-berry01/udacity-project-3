b4748c767d18ea19ff6e032b97bb7cfe472c0255
ol-berry01 <info@littlemonsterdigital.co.uk>
2021-01-22 12:08:11 +0000

    Generate button base functionality
    Event listener:
    - preventDefault
    - newZip set to zipCode.value
    - newFeelings set to feelings.value
    - log out Test
    
    Other variables:
    - baseUrl equal to openweathermap api
    - secretKey equal to openweathermap api key

--- website/app.js
+++ website/app.js
@@ -7,6 +7,17 @@ const temp = document.querySelector("#temp")
 const content = document.querySelector("#content")
 const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
+const baseUrl = "//api.openweathermap.org/data/2.5/weather?zip="
+const secretKey = "abff52ecfed597778c0d38f6b4faa03e"
+
 // Create a new date instance dynamically with JS
 let d = new Date()
 let newDate = month[d.getMonth()] + "." + d.getDate() + "." + d.getFullYear()
+
+generateBtn.addEventListener("click", e => {
+  e.preventDefault()
+  const newZip = zipCode.value
+  const newFeelings = feelings.value
+
+  console.log(`Test: ${newDate}, ${newZip}, ${newFeelings}`)
+})
